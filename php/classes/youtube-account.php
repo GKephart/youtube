5dbb2eb8914d42d990a6f81392620dafb07edeee
George Kephart <Gkephart@cnm.edu>
2015-10-24 13:39:12 -0600

    created Insert jquery template i have two thrown warnings that i need to ask Dylan about/

--- php/classes/youtube-account.php
+++ php/classes/youtube-account.php
@@ -26,7 +26,7 @@ class Account {
 	private $accountName;
 	/**
 	 * this is a basic description of the person who owns the account, this field is optional
-	 *@var string $userInfo
+	 * @var string $userInfo
 	 */
 	private $userInfo;
 	/**
@@ -74,14 +74,16 @@ class Account {
 			throw(new Exception($exception->getMessage(), 0, $exception));
 		}
 	}
+
 	/**
 	 * accessor method accountId
 	 *
 	 * @return mixed value of accountId
 	 */
-	public function getAccountId () {
+	public function getAccountId() {
 		return ($this->accountId);
 	}
+
 	/**
 	 * mutator method for accountId
 	 * @param mixed $newAccountId new value of account id
@@ -92,7 +94,7 @@ class Account {
 	public function setAccountId($newAccountId) {
 		// base case: if the account id is null, this is a new account without a mySQL assigned id
 		$newAccountId = filter_var($newAccountId, FILTER_VALIDATE_INT);
-		if($newAccountId === null){
+		if($newAccountId === null) {
 			$this->accountId = null;
 			return;
 		}
@@ -109,15 +111,17 @@ class Account {
 		// convert and store the profile id
 		$this->accountId = intval($newAccountId);
 	}
+
 	/**
 	 * Accessor method for email
 	 *
 	 * @return string value of email
 	 */
 
-	public  function getEmail() {
+	public function getEmail() {
 		return ($this->email);
 	}
+
 	/**
 	 * mutator method for email
 	 *
@@ -126,28 +130,30 @@ class Account {
 	 * @throws RangeException if $newHash is more than 128 characters
 	 */
 
-	public function setEmail ($newEmail){
+	public function setEmail($newEmail) {
 		// verify the email address is correct
 		$newEmail = trim($newEmail);
-		$newEmail = filter_var($newEmail,FILTER_SANITIZE_EMAIL);
+		$newEmail = filter_var($newEmail, FILTER_SANITIZE_EMAIL);
 		if(empty($newEmail) === true) {
 			throw(new InvalidArgumentException("user information is insecure or empty"));
 		}
 		// verify the email can fit into the database.
-		if(strlen($newEmail) > 128){
+		if(strlen($newEmail) > 128) {
 			throw(new RangeException("Email is to long"));
 		}
 		//store the tweet content
 		$this->email = $newEmail;
 	}
+
 	/**
 	 * accessor method for account name
 	 *
 	 * @return string value for account
 	 */
-	public function getAccountName () {
-		return($this->accountName);
+	public function getAccountName() {
+		return ($this->accountName);
 	}
+
 	/**
 	 * mutator method for account user name
 	 * @param string $newAccountName new value of of account user name
@@ -158,56 +164,59 @@ class Account {
 		// verify the account name content is secure
 		$newAccountName = trim($newAccountName);
 		$newAccountName = filter_var($newAccountName, FILTER_SANITIZE_STRING);
-		if(empty($newAccountName) === true){
+		if(empty($newAccountName) === true) {
 			throw(new InvalidArgumentException("Account name is empty or insecure"));
 		}
 
 		// verify the account name will fit into this database
-		if(strlen($newAccountName) > 10){
+		if(strlen($newAccountName) > 10) {
 			throw(new RangeException ("Account name is to large"));
 		}
 		// store the account name
 		$this->accountName = $newAccountName;
 	}
+
 	/**
 	 * Accessor method for user information
 	 *
 	 * @return string value of user information
 	 */
 
-	public  function getUserInfo(){
-		return($this->userInfo);
+	public function getUserInfo() {
+		return ($this->userInfo);
 	}
+
 	/**
 	 * Mutator method for user information
 	 *
 	 * @param string $newUserInfo new value of user information
 	 * @throws InvalidArgumentException if $newUserInfo is not a string or insecure
-	 *@throws RangeException if $newUserInfo is > 100 characters
+	 * @throws RangeException if $newUserInfo is > 100 characters
 	 */
-	public function setUserInfo ($newUserInfo){
+	public function setUserInfo($newUserInfo) {
 		// verify the user information is correct
 		$newUserInfo = trim($newUserInfo);
-		$newUserInfo = filter_var($newUserInfo,FILTER_SANITIZE_STRING);
+		$newUserInfo = filter_var($newUserInfo, FILTER_SANITIZE_STRING);
 
 		if(empty($newUserInfo) === true) {
 			throw(new InvalidArgumentException("user information is insecure or empty"));
 		}
 		// verify the tweet content will fit into the database.
-		if(strlen($newUserInfo) > 140){
+		if(strlen($newUserInfo) > 140) {
 			throw(new RangeException("user information is to long"));
 		}
 		//store the tweet content
 		$this->userInfo = $newUserInfo;
 	}
+
 	/**
 	 * Accessor method for salt
 	 *
 	 * @return string value of salt
 	 */
 
-	public  function getSalt(){
-		return($this->salt);
+	public function getSalt() {
+		return ($this->salt);
 	}
 
 	/**
@@ -215,18 +224,18 @@ class Account {
 	 *
 	 * @param string $newSalt new value of user information
 	 * @throws InvalidArgumentException if $newSalt is not a string or insecure
-	 *@throws RangeException if $newSalt is not 64 characters
+	 * @throws RangeException if $newSalt is not 64 characters
 	 */
 
-	public function setSalt ($newSalt){
+	public function setSalt($newSalt) {
 		// verify salt is correct
 		$newSalt = trim($newSalt);
-		$newSalt = filter_var($newSalt,FILTER_SANITIZE_STRING);
+		$newSalt = filter_var($newSalt, FILTER_SANITIZE_STRING);
 		if(empty($newSalt) === true) {
 			throw(new InvalidArgumentException("user salt information is insecure or empty"));
 		}
 		// verify salt is the correct length.
-		if(strlen($newSalt) !== 64){
+		if(strlen($newSalt) !== 64) {
 			throw(new RangeException("half of the password verification  is not the right length"));
 		}
 		//store the salt content
@@ -239,8 +248,8 @@ class Account {
 	 * @return string value of hash
 	 */
 
-	public  function getHash(){
-		return($this->hash);
+	public function getHash() {
+		return ($this->hash);
 	}
 
 	/**
@@ -251,27 +260,47 @@ class Account {
 	 * @throws RangeException if $newHash is not 128 characters
 	 */
 
-	public function setHash ($newHash){
+	public function setHash($newHash) {
 		// verify hash is correct
 		$newHash = trim($newHash);
-		$newHash = filter_var($newHash,FILTER_SANITIZE_STRING);
+		$newHash = filter_var($newHash, FILTER_SANITIZE_STRING);
 		if(empty($newHash) === true) {
 			throw(new InvalidArgumentException("user hash information is insecure or empty"));
 		}
 		// verify the hash will fit into the database.
-		if(strlen($newHash) !== 128 ){
+		if(strlen($newHash) !== 128) {
 			throw(new RangeException("half of the password verification  is not the right length"));
 		}
 		//store the hash
 		$this->hash = $newHash;
 	}
 
+	/**
+	 * Inserts this you tube account into mySQL
+	 *
+	 * @param PDO $pdo PDO connection object
+	 * @throws PDOException when mySQl errors occur
+	 */
 
+	public function insert(PDO $pdo) {
+		//enforce if the youTubeAccount is null
+		if($this->accountId !== null) {
+			throw(new PDOException ("not a new account"));
+		}
+		// create query template
+			$query
+				= "INSERT INTO youtubeAccount(email, accountName, userInfo, salt, hash) VALUES(:email, :accountname, :userInfo, :salt, :hash)";
+		 	$statement = $pdo->prepare($query);
 
+		 	// bind the member variables to the place holders in the template)
+		 	$parameters = array("email" => $this->email, "accountName" => $this->userInfo, "salt" => $this->salt, "hash" => $this->hash);
+		 $statement->excecute($parameters);
 
-
-
-
-
-
+		  // update the null accountId with with what mySQL just gave us
+		  $this->accountId = intval($pdo->LastInsertId());
+	}
 }
+
+
+
+
